4fda1e76cf0be2e6de93332df870ea19c184f0c8
Cory Dignard <43917703+corydignard@users.noreply.github.com>
2019-10-17 09:32:33 -0400

    Initial files
    Initial files for repo

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Cohort2-Windows-Config
-Configuration files and script to do basic configuration of Windows 10 workstations for Premium Cohort 2
+﻿# Configuration files and script to do basic configuration of Windows 10 workstations for Premium Cohort 2
+
+These configuration files are in support of the training courses the **Canada School of Public Service (CSPS) Digital Academy (DA) / École de la fonction publique du Canada (EFPC) Académie du numérique (AN)**, are hosting starting November, 2019.
+
+**Acknowledgements**
+Script template from [9to5IT/PS_Script_Template_V2_Logs.ps1]. This README.md was created using [dillinger.io]
+
+# Files
+* **DigitalAcademy-Installation.ps1**
+  * Sets: Power Plan, Time Zone, Desktop Wallpaper 
+  * Installs: Chrome, Slack Desktop
+* **Commands.txt** : List of commands I used to do the installs
+* **GoogleChromeStandaloneEnterprise64.msi** : Chrome offline install file
+* **SlackSetup.exe** : Slack Desktop install file
+* **PowerPlan.pow** : Windows Power Plan applied as part of the configuration
+* **wallpaper1920x1080.png** : Desktop wallpaper
+
+# How to use the scripts
+The Powershell scripts were designed on laptops running Windows 10 build 1903.
+
+* Download the following installations files
+  * Chrome bundle for Windows 64‑bit (GoogleChromeStandaloneEnterprise64.msi) - https://cloud.google.com/chrome-enterprise/browser/download/?h1=en
+  * Slack Desktop - https://slack.com/downloads/windows
+* Download and copy files to C:\DigitalAcademy\InstallFiles
+* Open Powershell prompt as administrator
+* Set Powershell execution policy to Unrestricted
+```sh
+Set-ExecutionPolicy Unrestricted
+select [A] Yes to All 
+```
+* Install PSLogging
+```sh
+Install-Module PSLogging
+select [A] Yes to All
+```
+* Launch installation script
+```sh
+C:\DigitalAcademy\InstallFiles\DigitalAcademy-Installation.ps1 -InstallFiles "c:\DigitalAcademy\InstallFiles"
+```
+* Restart workstation once complete
+* DONE!
+
+[dillinger.io]: <https://dillinger.io/>
+[9to5IT/PS_Script_Template_V2_Logs.ps1]: <https://gist.github.com/9to5IT/d81802b28cfd10ab5d89>
---
+++ Workstation Configuration/DigitalAcademy/InstallFiles/DigitalAcademy-Installation.ps1
@@ -0,0 +1,118 @@
+﻿#requires -version 4
+#Requires -RunAsAdministrator
+
+<#
+.SYNOPSIS
+  Configuration files and script to do basic configuration of Windows 10 workstations for Premium Cohort 2
+
+.DESCRIPTION
+  Sets: Power Plan, Time Zone, Desktop Wallpaper 
+  Installs: Chrome, Slack Desktop
+
+.PARAMETER InstallFiles
+  Folder where installation files are
+ 
+.INPUTS
+  None
+
+.OUTPUTS Log File
+  The script log file stored in $InstallFiles\DigitalAcademy-Installation.log
+
+.NOTES
+  Version:        1.0
+  Author:         Cory Dignard
+  Creation Date:  October 17, 2019
+  Purpose/Change: Initial script development
+
+
+.EXAMPLE
+  C:\DigitalAcademy\InstallFiles\DigitalAcademy-Installation.ps1 -InstallFiles "C:\DigitalAcademy\InstallFiles"
+  
+#>
+
+#---------------------------------------------------------[Script Parameters]------------------------------------------------------
+
+Param (
+  [Parameter(Mandatory=$true)][string]$InstallFiles
+)
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = 'SilentlyContinue'
+
+#Import Modules & Snap-ins
+Import-Module PSLogging
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Script Version
+$sScriptVersion = '1.0'
+
+#Log File Info
+$sLogPath = $InstallFiles
+$sLogName = 'DigitalAcademy-Installation.log'
+$sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+Function Write-Message ($message) {
+    write-host "`n$message" -ForegroundColor Green;write-host ""
+    Write-LogInfo -LogPath $sLogFile -Message "`n$message"
+
+}
+
+Function InstallSoftware ($Software,$InstallCommands,$Arguments) {
+
+  Begin {
+    Write-Message "Installing $Software"
+  }
+
+  Process {
+    Try {
+      Start-Process "$InstallCommands" -ArgumentList "$Arguments" -Wait -NoNewWindow
+    }
+
+    Catch {
+      Write-LogError -LogPath $sLogFile -Message $_.Exception -ExitGracefully
+      Break
+    }
+  }
+
+  End {
+    If ($?) {
+      write-Message "Completed."
+    }
+  }
+}
+
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+Start-Log -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion | Out-Null
+Write-Host "";"Log file: $sLogFile";Write-Host ""
+
+Write-Message "Setting up Power Plan"
+powercfg /import "$InstallFiles\PowerPlan.pow"
+$HighPerf = powercfg -l | %{if($_.contains("POWER")) {$_.split()[3]}}
+powercfg -setactive $HighPerf
+Write-Message "Completed"
+
+Write-Message "Set Time Zone"
+Set-TimeZone -Id "Eastern Standard Time"
+Write-Message "Completed"
+
+Write-Message "Installing Applications"
+InstallSoftware "Chrome" "msiexec" "/i $InstallFiles\GoogleChromeStandaloneEnterprise64.msi /q"
+InstallSoftware "Slack" "$InstallFiles\SlackSetup.exe" "/silent"
+Write-Message "Completed"
+
+Write-Message "Set Desktop Wallpaper"
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "$InstallFiles\wallpaper1920x1080.png"
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name WallpaperStyle -value "6"
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+Write-Message "Completed"
+
+Write-Message "All tasks completed."
+
+Stop-Log -LogPath $sLogFile
